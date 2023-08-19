cd5f12c1bdb061469ca763755983b6b89e4c4a8e
nandhu10jishnu <124574524+nandhu10jishnu@users.noreply.github.com>
2023-08-19 06:58:11 +0530

    Delete src directory
    delete src

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-
-const root = document.getElementById('root');
-const rootElement = React.createElement(App);
-ReactDOM.createRoot(root).render(rootElement);
